4f56eca1c508ad6932bb6e1552f9f842f8abae97
slebreault <samuellebreault@yahoo.com>
2016-11-26 23:26:12 -0500

    oops

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,2 @@
-curl https://demo.docusign.net/restapi/v2/login_information
--i -k -H "Accept: application/json"
--H "Content-Type: application/json"
--H "X-DocuSign-Authentication: <DocuSignCredentials><Username>samuellebreault.com</Username><Password>hamandcheese</Password><IntegratorKey>058a2d69-6885-4538-ba40-e6a47bc55d23</IntegratorKey></DocuSignCredentials>"
 
 
